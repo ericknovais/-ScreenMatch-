693e7b969a56b9e22e8122149750bd99a6828db3
ericknovais <erick.hora@outlook.com>
2024-08-29 18:24:42 -0300

    Primeiro commint

---
+++ screenmatch/src/test/java/br/com/endsystem/screenmatch/ScreenmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.endsystem.screenmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
